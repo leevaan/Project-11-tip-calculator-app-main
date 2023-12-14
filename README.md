c07c59fc829e584f11523d96ff8abfe5d587813f
Levani Jikia <jiqiaa@gmail.com>
2023-12-15 00:11:20 +0400

    s
    s

---
+++ README.md
@@ -0,0 +1,2 @@
+# Project-11-tip-calculator-app-main
+ 
---
+++ script.js
@@ -0,0 +1,161 @@
+"use strict"
+const billClass = document.querySelector("#usdd");
+const customClass = document.querySelector(".custom");
+const personClass = document.querySelector("#personn");
+
+const amount = document.querySelector(".amount-value");
+const totalPerson = document.querySelectorAll(".amount-value")[1];
+
+const billBorder = document.querySelectorAll(".input-div")[0];
+const personBorder = document.querySelectorAll(".input-div")[1];
+
+const button = document.getElementsByTagName("button")[0];
+const tipClass = document.querySelectorAll(".tip");
+
+let tip, total, billValue, personValue, customValue, numClass;
+console.log("🚀 ~ file: script.js:78 ~ personClass.addEventListener ~ personValue:", personValue)
+// bill border style.
+billClass.addEventListener("focus", () => {
+    billBorder.style.border = "2px solid #26C2AE";
+    document.querySelector(".usd-path").style.fill = "#00474B";
+});
+billClass.addEventListener("blur", () => {
+    billBorder.style.borderColor = "transparent";
+    document.querySelector(".usd-path").style.fill = "#9EBBBD";
+});
+
+// custom border style.
+customClass.addEventListener("focus", () => {
+    customClass.style.border = "2px solid #26C2AE";
+});
+customClass.addEventListener("blur", () => {
+    customClass.style.borderColor = "transparent";
+});
+
+//person border style
+personClass.addEventListener("focus", () => {
+    if(personClass.value != 0 || personClass.value == ""){
+        personBorder.style.border = "2px solid #26C2AE";
+        document.querySelector(".person-path").style.fill = "#00474B";
+    }
+   
+});
+console.log("🚀 ~ file: script.js:44 ~ personClass.value:", personClass.value)
+personClass.addEventListener("blur", () => {
+    // personClass.value = undefined;
+    if(personClass.value != 0 || personClass.value == ""){
+        personBorder.style.borderColor = "transparent";
+        document.querySelector(".person-path").style.fill = "#9EBBBD";
+    }
+        
+    });
+
+//ვიმახსოვრებთ ველიუში ჩაწერილ ციფრს და სამი ველიუდან ( Bill, precent, person) თუ ბოლოს შევსო bill ველიუ ეს გაუშვებს დავთვლის ფუნქციას 
+billClass.addEventListener("input", () =>{
+    billClass.style.background = "transparent";
+    button.style.backgroundColor = "#26C2AE";
+    billValue = billClass.value;
+
+    if(billValue == ""){
+        button.style.backgroundColor = "";
+    }
+    if(personValue > 0 && billValue > 0 && numClass != undefined){
+        condition(billValue, numClass, personValue);
+    }
+});
+
+//ვიმახსოვრებთ ველიუში ჩაწერილ ციფრს და სამი ველიუდან ( Bill, precent, person) თუ ბოლოს შევსო person ველიუ ეს გაუშვებს დავთვლის ფუნქციას 
+personClass.addEventListener("input", () =>{
+    personClass.style.background = "transparent";
+    personValue = personClass.value;
+    button.style.backgroundColor = "#26C2AE";
+
+    if(personClass.value == 0){
+        personBorder.style.border = "2px solid #E17052";
+        document.querySelector(".person-alert").style.display = "inline";
+        document.querySelector(".person-path").style.fill = "#E17052";
+    } 
+    if(personClass.value != 0 || personClass.value == ""){
+        
+        personBorder.style.border = "2px solid #26C2AE";
+        document.querySelector(".person-alert").style.display = "none";
+        document.querySelector(".person-path").style.fill = "#00474B";
+    } 
+    if(personValue == ""){
+        button.style.backgroundColor = "";
+    }
+
+    if(personValue > 0 && billValue > 0 && numClass != undefined){
+        condition(billValue, numClass, personValue);
+    }
+});
+// როდესაც ფიქსირებულ პროცენტს ირჩევს.
+document.querySelector(".left-middle-cont").addEventListener("click", event => {
+    numClass = event.target.getAttribute("num");
+    let eventTarget = event.target;
+   
+    if(eventTarget.classList[1] == undefined && eventTarget.classList[0] == "tip")
+    {    
+        for(let i = 0; i < tipClass.length; i++){
+            tipClass[i].classList.remove("show");
+        }
+        customClass.value = "";
+        eventTarget.classList.add("show");
+        button.style.backgroundColor = "#26C2AE";
+      }else if(eventTarget.classList[1] == "show"){   
+        eventTarget.classList.remove("show");
+        button.style.backgroundColor = "";
+        numClass = null;
+        amount.innerHTML = "$0.00";
+        totalPerson.innerHTML = "$0.00";
+    }
+
+    if(personValue > 0 && billValue > 0 && numClass != null){
+        condition(billValue, numClass, personValue);
+    }
+});
+
+// კოსტუმ ველიუი. იმახსოვრებს შეყვანილს ციფრს.შეყვანილ ციფრს სტრინგად აქცევს ნომრად და თუ სასვსე დანარჩენი ორი ველი იძახებს გამოთვლის ფუნქციას.
+customClass.addEventListener("input", () => {
+    customValue = customClass.value;
+    button.style.backgroundColor = "#26C2AE";
+    for(let i = 0; i < tipClass.length; i++){
+        tipClass[i].classList.remove("show");
+    }
+    if(typeof Number(customClass.value) == "number" && personClass.value > 0 && billClass.value > 0 && customClass.value != ""){
+        condition(billValue, customValue, personValue);
+    }else if(customValue == ""){ //თუ მომხმარებელი ქასთმ ველიუში ჩაწერილ ციფრებს წაშლის უნდა განულდეს ტიპ და ტოტალ ველიუები
+        button.style.backgroundColor = "";
+        totalPerson.innerHTML = "$0.00";
+        amount.innerHTML = "$0.00";
+    }else{
+        numClass = customValue;
+    }
+});
+
+// კალკულატორის მათემატიკური პირობა.
+const condition = (bill, custom, person) => {
+        tip = (bill * custom / 100) / person;
+        amount.innerHTML ="$" + tip.toFixed(2);
+
+        total = (bill / person) + tip;
+        totalPerson.innerHTML = "$" + total.toFixed(2);
+}
+
+//reset button
+button.addEventListener("click", () => {
+    amount.innerHTML = "$0.00";
+    totalPerson.innerHTML = "$0.00";
+    billValue= undefined;
+    personValue = undefined;
+    personClass.value = "";
+    billClass.value = "";
+    customClass.value = "";
+    for(let i = 0; i < tipClass.length; i++){
+        tipClass[i].classList.remove("show");
+    }
+    button.style.backgroundColor = "";
+    personBorder.style.borderColor = "transparent";
+    document.querySelector(".person-alert").style.display = "none";
+    document.querySelector(".person-path").style.fill = "#9EBBBD";
+});
